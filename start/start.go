99ccc532e9b5256609350ed905600bcec3406735
o-t-k-t <wetcondition@gmail.com>
2022-05-01 17:15:56 +0900

    query methods

--- start/start.go
+++ start/start.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/o-t-k-t/ent-exercise/ent"
+	"github.com/o-t-k-t/ent-exercise/ent/user"
 
 	_ "github.com/lib/pq"
 )
@@ -20,4 +22,31 @@ func main() {
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+}
+
+func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Create().
+		SetAge(30).
+		SetName("a8m").
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating user: %w", err)
+	}
+	log.Println("user was created: ", u)
+	return u, nil
+}
+
+func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+	u, err := client.User.
+		Query().
+		Where(user.Name("a8m")).
+		// ユーザーが見つからない場合、`Only`は失敗する
+		// あるいは、1人以上のユーザーが返却される
+		Only(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed querying user: %w", err)
+	}
+	log.Println("user returned: ", u)
+	return u, nil
 }
